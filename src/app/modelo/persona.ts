3afcbc22f84cb8574cab27e985bddf6ac9ea4a26
utercero <utercero@gmail.com>
2020-09-04 12:03:15 +0200

    modelo

--- src/app/modelo/persona.ts
+++ src/app/modelo/persona.ts
@@ -1,2 +1,18 @@
 export class Persona {
+    private static personas = 0;
+    constructor(public nombre:string,public apellido:string,public id:number=-1){
+        Persona.personas++;
+        if(this.id==-1)
+            this.id = Persona.personas;
+    }
+    clonar():Persona{
+        return new Persona(this.nombre,this.apellido,this.id);
+    }
+    public static fromJson (data): Persona {
+                   
+        if(!data.id || !data.nombre || data.apellido){                
+           throw (new Error("Invalid argument structure do not match with model fields"));
+        }        
+        return new Persona(data.nombre,data.apellido,data.id);
+    }
 }
